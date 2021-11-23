df2d8544cc1f51a0ab1c757438adb0465e52d9c3
A. Korobetskaya <kornast@yandex.ru>
2021-11-23 13:26:22 +0400

    Добавлены appsettings со строкой подключения

--- MurrrcatConsoleCodeFirst/Program.cs
+++ MurrrcatConsoleCodeFirst/Program.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
 
 namespace MurrrcatConsoleCodeFirst
 {
     class Program
     {
         static readonly string connectionString = new ConnectionStringManager().ConnectionString;
+
         static void Main(string[] args)
         {
-
+            
         }
     }
 }
